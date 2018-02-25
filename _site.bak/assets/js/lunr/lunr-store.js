fb9d93554c5a5306def0ef5b74f3549d80a8b243
Git_Tammy <34313266@qq.com>
2018-02-25 17:56:44 +0800

    remove unuse

---
+++ _site.bak/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Hello World!",
+        "excerpt":"Hello World!backstreettoy/assets/img通过在_posts文件夹中增加文件来建立文章如果要使用草稿功能，在_drafts文件夹中增加文章，默认情况下草稿不会发布到网站上，如果需要发布，在参数中增加 –drafts，例如jekyll serve –incremental –drafts如果要插入图片，将文件拷贝到 assets/img 文件夹中，然后通过下列的方式引用图片&lt;img src=”/assets/img/filename “ /&gt;","categories": [],
+        "tags": [],
+        "url": "/helloworld/",
+        "teaser":null}]
